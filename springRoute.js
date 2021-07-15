9662e0d0757a9a5b93cbca7ec1a3e1b39e217037
Phat Nguyen <phatnguyen2499@gmail.com>
2021-07-15 10:36:48 +0700

    change

--- springRoute.js
+++ springRoute.js
@@ -1,4 +1,4 @@
-var root = "http://localhost:8080";
+var root = "https://d37qwhg1wwqiqd.cloudfront.net";
 var spring = {
   restaurant: `${root}/restaurants`,
   restaurantEdit: `${root}/restaurants/edit`,
